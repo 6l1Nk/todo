07c05600f2f415c4076db2c2863ea468b321139c
shablabla <miranda@mcdonalds.gov>
2024-04-19 09:54:39 -0500

    initial args handling

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+ //   "bufio"
+    "fmt"
+    "os"
+   // "log"
+)
+
+
+//func addToDo(todoFile string, todo string) {
+  //  fmt.Println("adding")
+    //file, err := os.OpenFile(todoFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+ //   if err != nil {
+   //     log.Fatalf("Failed to open file: %v", err)
+ //   }
+   // defer file.Close()
+
+ //   if _, err := file.WriteString(todo + "\n"); err != nil {
+      //  log.Fatalf("Failed to write to file: %v", err)
+    //}
+  //  fmt.Println(todo)
+//}
+
+//func completeToDo(todoFile string) {
+ //   fmt.Println("completing")
+   // inputFile, err := os.Open(todoFile)
+ //   if err != nil {
+	   // log.Fatalf("Failed to open file: %v", err)
+   // }
+
+ //   tempFileName := todoFile + ".tmp"
+   // tempFile, err := os.Create(tempFileName)
+ //   if err != nil {
+	 //   log.Fatalf("Failed horrifically %v", err)
+ //   }
+   // defer tempFile.Close()
+
+ //   scanner := bufio.NewScanner(inputFile)
+   // scanner.Scan()
+ //   for scanner.Scan() {
+	  //  _, err := tempFile.WriteString(scanner.Text() + "\n")
+	//    if err != nil {
+		 //   log.Fatalf("what the heck happnd?, %v", err)
+	 //   }
+ //   }//
+
+    //if err := scanner.Err(); err != nil {
+	//    log.Fatalf("oh my gaaaawddd!! %v", err)
+ //   }
+
+ //   inputFile.Close()
+   // err = os.Remove(todoFile)
+ //   if err != nil {
+	   // log.Fatalf("snapple dawg $v", err)
+   // }
+
+ //   err = os.Rename(tempFileName, todoFile)
+    //if err != nil {
+	//    log.Fatalf("renamin ain wut it usd 2b %v", err)
+ //   }
+
+//}
+
+//func whatsNext(todoFile string) {
+    //fmt.Println("What's next?")
+
+    //file, err := os.Open(todoFile)
+    //if err != nil {
+	    //fmt.Println("Error:", err)
+	//    return
+    //}
+ //   defer file.Close()
+
+   // scanner := bufio.NewScanner(file)
+    //if scanner.Scan() {
+	    //return scanner.Text()
+    //}
+ //   return todoFile
+//}
+
+//func deferToDo(todoFile string) {
+     // nextTask := whatsNext(todoFile)
+    //addToDo("whats next")
+    //completeToDo()
+ //   return todoFile
+//}
+
+func main() {
+    if len(os.Args) > 1 {
+        command := os.Args[1]
+	todoFile := "/home/nestor/dev/todo/todo.txt"
+  fmt.Println(todoFile) 
+        switch command {
+        case "--add":
+            if len(os.Args) > 2 {
+              //  addToDo(todoFile, os.Args[2])
+            } else {
+                fmt.Println("Nothing to add...")
+            }
+        case "--complete":
+            //completeToDo(todoFile)
+        case "--defer":
+            //deferToDo(todoFile)
+        case "--whatnext":
+            //fmt.Println(whatsNext(todoFile))
+        default:
+            fmt.Println("invalid argument...")
+            return
+        }
+    }
+}
+
+
